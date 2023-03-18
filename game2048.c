282e247f6a950605728623d380d826de80a4c9a7
YUAN Quan <yq970710@gmail.com>
2023-03-17 18:00:28 -0700

    overall structure setup

---
+++ game2048.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <ncurses.h>
+
+// global variable
+int board[4][4];
+int emptyCell;
+int highestNum;
+int totalScore;
+
+// Model part
+
+
+
+
+// View part
+
+
+
+// Control part
+
+int main(){
+    
+    return 0;
+}
+
+
