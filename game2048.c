aa31b1ddff994b33a748c4991836835eaf21f8f1
Lingjing_Liu <55669289+OliviaLiu123@users.noreply.github.com>
2023-04-12 19:09:36 -0700

    updated add resume feature

--- game2048.c
+++ game2048.c
@@ -4,43 +4,70 @@
 #include <string.h>
 #include <time.h>
 //#include <ncurses.h>
+#include <unistd.h>
 
 // global variable
 int N = 4;
 int board[4][4];
 int emptyCell = 16;
-int highestNum;
-int totalScore;
+int highestNum = 0;
+int totalScore = 0;
+int sleepTime = 400000;
+bool started = false;
+
+// function name
+// helper functions
+bool canOperate();
+bool canOperateUp();
+bool canOperateDown();
+bool canOperateLeft();
+bool canOperateRight();
+bool isAlive();
+bool win();
+int newNum();
+void generateNewCell();
+// move functions
+void rotate(int times);
+void moveToLast(int col, int rowa, int rowb);
+void moveAndMerge();
+void moveUp();
+void moveDown();
+void moveLeft();
+void moveRight();
+// print functions
+void printDisplay();
+void viewbar();
+
+// comprehensive functions
+void showInfo();
+void slideUP();
+void slideDOWN();
+void slideLEFT();
+void slideRIGHT();
+
 
 // Model part
 
-void printDisplay();
-void test();
-void clearScreen() {
-#ifdef _WIN32
-    // Windows platform
-    system("cls");
-#elif __APPLE__
-    // macOS platform
-        system("clear");
-#else
-    // Other platforms (e.g., Linux)
-    system("clear");
-#endif
+
+// check if the game is over
+bool isAlive() {
+    return emptyCell>0 || canOperate();
 }
 
 // check if there can be another operation
 // when the board is full
 bool canOperate() {
-    // check if there is a cell with same num on the right/down side
-    // of each cell
+    return canOperateUp() || canOperateDown()
+           || canOperateLeft() || canOperateRight();
+}
+
+// check if the board will be different if move up
+bool canOperateUp() {
     int i, j;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            if (j < 3 && board[i][j] == board[i][j + 1]) {
-                return true;
-            }
-            if (i < 3 && board[i][j] == board[i + 1][j]) {
+    for (i=1; i<N; i++) {
+        for (j=0; j<N; j++) {
+            if (board[i][j] > 0 &&
+                (board[i-1][j] == board[i][j] || board[i-1][j] == 0)){
                 return true;
             }
         }
@@ -48,28 +75,71 @@ bool canOperate() {
     return false;
 }
 
-// check if the game is over
-bool isAlive() {
-    if (emptyCell > 0) {
-        return true;
+// check if the board will be different if move down
+bool canOperateDown() {
+    int i, j;
+    for (i=0; i<N-1; i++) {
+        for (j=0; j<N; j++) {
+            if (board[i][j] > 0 &&
+                (board[i+1][j] == board[i][j] || board[i+1][j] == 0)){
+                return true;
+            }
+        }
     }
-    return canOperate();
+    return false;
 }
+
+// check if the board will be different if move left
+bool canOperateLeft() {
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=1; j<N; j++) {
+            if (board[i][j] > 0 &&
+                (board[i][j-1] == board[i][j] || board[i][j-1] == 0)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// check if the board will be different if move right
+bool canOperateRight() {
+    int i, j;
+    for (i=1; i<N; i++) {
+        for (j=0; j<N-1; j++) {
+            if (board[i][j] > 0 &&
+                (board[i][j+1] == board[i][j] || board[i][j+1] == 0)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 
 // check if the highest number reach 2048
 bool win() {
     return highestNum == 2048;
 }
 
-// generate a new number between 2 & 4, with equal probability
+// generate a new number between 2 & 4
+// probability: 2 - 75%, 4 - 25%
 int newNum() {
-    return (rand() % 2 + 1) * 2;
+    int p = rand() % 4;
+    if (p<3){
+        return 2;
+    } else {
+        return 4;
+    }
 }
 
 // among all the empty cell
 // draw a random cell and fill the cell with a number
 void generateNewCell() {
     // if there is no more empty cell, stop generating
+    // but not fail the game because need to check if it can be slide
+    // form other directions
     if (emptyCell == 0) {
         return;
     }
@@ -78,12 +148,13 @@ void generateNewCell() {
     // fill the random number th empty cell
     int randNum = rand() % emptyCell;
     int i, j;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            if (board[i][j] == 0) {
-                if (randNum == 0) {
+    for (i=0; i<N; i++){
+        for (j=0; j<N; j++){
+            if (board[i][j] == 0){
+                if (randNum == 0){
                     board[i][j] = newNum();
                     emptyCell--;
+                    highestNum = board[i][j] > highestNum? board[i][j] : highestNum;
                     return;
                 }
                 randNum--;
@@ -101,11 +172,11 @@ void rotate(int times) {
     while (times-- > 0) {
         for (int i = 0; i < N / 2; i++) {
             for (int j = i; j < N - i - 1; j++) {
-                int temp = board[i][j]; 
+                int temp = board[i][j];
                 board[i][j] = board[N - j - 1][i];
-                board[N - j - 1][i] = board[N - i - 1][N - j - 1]; 
-                board[N - i - 1][N - j - 1] = board[j][N - i - 1]; 
-                board[j][N - i - 1] = temp; 
+                board[N - j - 1][i] = board[N - i - 1][N - j - 1];
+                board[N - i - 1][N - j - 1] = board[j][N - i - 1];
+                board[j][N - i - 1] = temp;
             }
         }
     }
@@ -113,8 +184,13 @@ void rotate(int times) {
 
 // move tile board[rowb][col] to board[rowa][col], merge when possible
 void moveToLast(int col, int rowa, int rowb) {
-    if (rowa == rowb) return; 
-    board[rowa][col] += board[rowb][col]; 
+    if (rowa == rowb) return;
+    if (board[rowa][col] != 0){
+        // means merge a new one into this one
+        emptyCell++;
+    }
+    board[rowa][col] += board[rowb][col];
+    highestNum = board[rowa][col] > highestNum ? board[rowa][col] : highestNum;
     board[rowb][col] = 0;
 }
 
@@ -122,27 +198,27 @@ void moveAndMerge() {
     for (int col = 0; col < N; col++) {
         int last = 0;   // last is the row index for last tile
 
-        bool foundFirst = false; 
-        bool canMerge = true; 
+        bool foundFirst = false;
+        bool canMerge = true;
         for (int row = 0; row < N; row++) {
-            // skip if current block is not occupied 
-            if (board[row][col] == 0) continue; 
-            // base case: if we found the first tile, 
+            // skip if current block is not occupied
+            if (board[row][col] == 0) continue;
+            // base case: if we found the first tile,
             // we move the tile to row 0
             if (!foundFirst) {
-                foundFirst = true; 
-                moveToLast(col, last, row); 
+                foundFirst = true;
+                moveToLast(col, last, row);
                 continue;
             }
             // general case: compare curr tile to last tile
             // if they can be merged, merge them
             // otherwise move curr tile to last tile's next tile
             if (canMerge && board[last][col] == board[row][col]) {
-                moveToLast(col, last, row); 
-                canMerge = false;   // we can't merge to a tile that's already been merged 
-                totalScore += board[last][col]; 
+                moveToLast(col, last, row);
+                canMerge = false;   // we can't merge to a tile that's already been merged
+                totalScore += board[last][col];
             } else {
-                moveToLast(col, ++last, row); 
+                moveToLast(col, ++last, row);
                 canMerge = true;
             }
         }
@@ -167,30 +243,118 @@ void moveLeft() {
 }
 
 void moveRight() {
-    rotate(3); 
-    moveAndMerge(); 
+    rotate(3);
+    moveAndMerge();
     rotate(1);
 }
 
 
-
 // View part
 
+//save game state and resume the game
+typedef struct game_state{
+    int board[4][4];
+    int highestNum;
+    int totalScore;
+    int emptyCell;
+}Game_states;
 
+// save the game states to a file
+
+//write file
+void save_game_in_file(const Game_states* game){
+    FILE * file=fopen("game_state.txt","w");
+    if(file==NULL){
+        printf("Error:Unable to save the game state\n");
+        return;
+    }
+   size_t items_written= fwrite(game,sizeof(Game_states),1,file);
+    if (items_written != 1) {
+        perror("Error: fwrite failed");
+    }
+    fclose(file);
+}
+
+//load game
+//if can find and read the file return true, otherwise return false
+bool load_game(Game_states* game){
+    FILE * file=fopen("game_state.txt","r");
+    if(file==NULL){
+        printf("Error:Unable to load the game state\n");
+        return false;
+    }
+    size_t items_read=fread(game,sizeof(Game_states),1,file);
+    fclose(file);
+
+    if(items_read!=1){
+        printf("Error:Incorrect number of items read from the game.\n");
+        return false;
+    }
+
+    return true;
+}
+
+//
+void save_game(){
+    printf("Saving game...\n");
+    Game_states* game=malloc(sizeof(Game_states));
+    memcpy(game->board,board,sizeof(board));
+    game->highestNum=highestNum;
+    game->totalScore=totalScore;
+    game->emptyCell=emptyCell;
+    save_game_in_file(game);
+    printf("Game saved!\n");
+    free(game);
+}
+
+//resume game
+void resume_game(){
+    Game_states* game=malloc(sizeof(Game_states));
+    printf("Loading game..\n");
+    if(load_game(game)){
+        memcpy(board,game->board,sizeof(board));
+        highestNum=game->highestNum;
+        totalScore=game->totalScore;
+        emptyCell=game->emptyCell;
+        showInfo();
+        printf("Game loaded!\n");
+
+    }else{
+        printf("Failed to load game.\n");
+    }
+    free(game);
+}
+
+
+// detect system and call corresponding terminal command
+void clearScreen() {
+#ifdef _WIN32
+    // Windows platform
+    system("cls");
+#elif __APPLE__
+    // macOS platform
+        system("clear");
+#else
+    // Other platforms (e.g., Linux)
+    system("clear");
+#endif
+}
+
+// printt the board
 void printDisplay() {
     //top of the board
     printf("+");
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             printf("-");
         }
         printf("+");
     }
     printf("\n");
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < N; i++) {
         printf("|");
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < N; j++) {
             if (board[i][j] == 0) {
                 for (int k = 0; k < 4; k++) {
                     printf(" ");
@@ -203,11 +367,11 @@ void printDisplay() {
                     numWidth++;
                     n /= 10;
                 }
-                for (int k = 0; k < (4 - numWidth) / 2; k++) {
+                for (int k = 0; k < (N - numWidth) / 2; k++) {
                     printf(" ");
                 }
                 printf("%d", board[i][j]);
-                for (int k = 0; k < (4 - numWidth + 1) / 2; k++) {
+                for (int k = 0; k < (N - numWidth + 1) / 2; k++) {
                     printf(" ");
                 }
             }
@@ -215,8 +379,8 @@ void printDisplay() {
         }
         //bottom of the board
         printf("\n+");
-        for (int j = 0; j < 4; j++) {
-            for (int k = 0; k < 4; k++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
                 printf("-");
             }
             printf("+");
@@ -225,108 +389,156 @@ void printDisplay() {
     }
 }
 
+// print the score information
 void viewbar() {
-
     printf("Total score:%d\n", totalScore);
     printf("Highest number: %d\n", highestNum);
+}
 
+
+
+// Control part
+
+// clear the page and then print the viewbar and board
+void showInfo(){
+    clearScreen();
+    viewbar();
+    printf("\n");
+    printDisplay();
+}
+
+// for major function and outprint for 4 direction slide
+// show the merge result first
+// wait for a while then generate the new cube
+void slideUP(){
+    if (!canOperateUp()){
+        return;
+    }
+    moveUp();
+    showInfo();
+    printf("UP\n");
+    usleep(sleepTime);
+    generateNewCell();
+    showInfo();
+    printf("UP\n");
+}
+
+
+void slideDOWN(){
+    if (!canOperateDown()){
+        return;
+    }
+    moveDown();
+    showInfo();
+    printf("DOWN\n");
+    usleep(sleepTime);
+    generateNewCell();
+    showInfo();
+    printf("DOWN\n");
+}
+
+
+void slideLEFT(){
+    if (!canOperateLeft()){
+        return;
+    }
+    moveLeft();
+    showInfo();
+    printf("LEFT\n");
+    usleep(sleepTime);
+    generateNewCell();
+    showInfo();
+    printf("LEFT\n");
+}
+
+
+void slideRIGHT(){
+    if (!canOperateRight()){
+        return;
+    }
+    moveRight();
+    showInfo();
+    printf("RIGHT\n");
+    usleep(sleepTime);
+    generateNewCell();
+    showInfo();
+    printf("RIGHT\n");
 }
 
 
 int main() {
 
-    //0. Prepare the variables
+    //1. print starting message
+    printf("Welcome to the 2048 game!\n\n");
+    printf("Here are the instructions:\n\n");
+    printf("Press # to start the game or ESC to quit\n");
+    printf("Press w to slide UP\n");
+    printf("Press s to slide DOWN\n");
+    printf("Press a to slide LEFT\n");
+    printf("Press d to slide RIGHT\n");
+    printf("Press l to Resume game\n");
 
-    //1. display the board
-    viewbar();
-    printDisplay();
-    printf("Push '#' to start the game or 'ESC' to quit\n");
-    while (isAlive()) {
-
+    while (isAlive() && !win()) {
         //receiving [wasd]
         int ch1, ch2;
         ch1 = getchar();
-        if (ch1 == 27)
+        if (ch1 == 27) {
             break;
-        else {
+        } else {
             ch2 = getchar();  //receiving "\n"
             // switch case
             switch (ch1) {
-                case '#':  //start the game
-                    clearScreen();
-                    viewbar();
-                    generateNewCell();
-                    printDisplay();
+                case '#':
+                    //start the game
+                    if (!started){
+                        started = true;
+                        generateNewCell();
+                        generateNewCell();
+                        showInfo();
+                    } else {
+                        printf("The game already started\n");
+                    }
                     break;
                 case  'w':
-                    moveUp();
-                    clearScreen();
-                    viewbar();
-                    generateNewCell();
-                    printDisplay();
-                    printf("UP");
+                    slideUP();
                     break;
                 case 's':
-                    moveDown();
-                    clearScreen();
-                    viewbar();
-                    generateNewCell();
-                    printDisplay();
-                    printf("DOWN");
+                    slideDOWN();
                     break;
                 case 'a':
-                    moveLeft();
-                    clearScreen();
-                    viewbar();
-                    generateNewCell();
-                    printDisplay();
-                    printf("LEFT");
+                    slideLEFT();
                     break;
                 case 'd':
-                    moveRight();
-                    clearScreen();
-                    viewbar();
-                    generateNewCell();
-                    printDisplay();
-                    printf("RIGHT");
+                    slideRIGHT();
+                    break;
+                case 'l':
+                    if (!started){
+                        started = true;
+                        resume_game();
+                    } else {
+                        printf("The game already started, cannot resume\n");
+                    }
                     break;
                 default:
-                    break;
-            }
-            if (win()) {
-                printf("Congratulations! you win.\n");
+                    printf("Illeagal input! Please use wsad\n");
             }
         }
     }
 
+    if (win()){
+        printf("YOU WIN!\n");
+    } else if (!isAlive()){
+        printf("YOU LOSE!\n");
+    } else {
+        printf("Do you want to save the current status? (Y/N)\n");
+        char ans;
+        scanf("%s",&ans);
+
+        if(ans=='Y' || ans=='y'){
+            save_game();
+        }
+
+
+    }
     return 0;
 }
-
-void test() {
-        int i = 0;
-    int j=0;
-    for (i=0; i<4; i++){
-        // if (i == 1) continue; 
-        for (j=0; j<2; j++){
-            board[i][j] = 2;
-        }
-        for (j = 2; j < 4; j++) {
-            board[i][j] = 4; 
-        }
-    }
-    viewbar();
-    printDisplay();
-
-    moveUp(); 
-    printDisplay();
-
-    moveLeft(); 
-    printDisplay(); 
-
-    moveRight(); 
-    printDisplay(); 
-
-    moveDown(); 
-    printDisplay(); 
-}
-
